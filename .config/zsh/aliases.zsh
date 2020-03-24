6d483b0efec515c5acca29165b6f4d7675a157d0
George Witteman <george@witteman.me>
2020-03-23 18:51:26 -0700

    Make gb command use fzf

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -27,7 +27,6 @@ alias la='ls -la'
 compdef g='git'
 alias gs='git status'
 alias gf='git fetch && g'
-alias gb="git branch --sort=-committerdate --format='%(if)%(HEAD)%(then)* %(color:bold green)%(else)  %(end)%(refname:short)%(color:nobold normal) (%(committerdate:relative))'"
 alias develop='master'
 
 # yadm
