5f9d018e72c7d368055ed1e09406233666e8cb24
George Witteman <george@witteman.me>
2020-06-06 00:02:39 -0700

    Fix .fzf.zsh script on linux

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,21 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]] && [[ -d /usr/local/opt/fzf/bin ]]; then
   export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+if [[ -f "/usr/local/opt/fzf/shell/completion.zsh" ]]; then
+  [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+elif [[ -f "/usr/share/fzf/completion.zsh" ]]; then
+  [[ $- == *i* ]] && source "/usr/share/fzf/completion.zsh" 2> /dev/null
+fi
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+if [[ -f "/usr/local/opt/fzf/shell/key-bindings.zsh" ]]; then
+  source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+elif [[ -f "/usr/share/fzf/key-bindings.zsh" ]]; then
+  source "/usr/share/fzf/key-bindings.zsh"
+fi
