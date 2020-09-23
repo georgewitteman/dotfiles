12fc2883f1f8aa18446643da2080d70885ca8a79
George Witteman <george.witteman@affirm.com>
2020-09-22 22:22:46 -0700

    Path cleanup

---
+++ private_dot_config/sh/exact_pyenv/init.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+pyenv() {
+  command pyenv "$@" || return "$?"
+
+  while true; do
+    case ":$PATH" in
+      *:"$PYENV_ROOT"/*:*) PATH="${PATH%%${PYENV_ROOT}*}${PATH#*${PYENV_ROOT}*:}" ;;
+      *"$PYENV_ROOT"/*) PATH="${PATH%%${PYENV_ROOT}*}" ;;
+      *)
+        PATH="${PATH%:}"
+        break
+        ;;
+    esac
+  done
+
+  while IFS= read -r line || [ -n "$line" ]; do
+    PYENV_PATH="${PYENV_PATH:+${PYENV_PATH}:}${PYENV_ROOT}/versions/${line}/bin"
+  done <"${PYENV_ROOT}/version"
+
+  # Reverse the path so that the first version in the file is first in $PATH
+  PATH="${PYENV_PATH:+${PYENV_PATH}:}${PYENV_ROOT}/bin:${PATH}"
+  unset PYENV_PATH NEW_PATH
+}
--- private_dot_config/sh/exact_pyenv/profile.sh
+++ private_dot_config/sh/exact_pyenv/profile.sh
@@ -2,8 +2,10 @@
 
 export PYENV_ROOT="${HOME}/.local/share/yavm/pyenv"
 
-PATH="${PYENV_ROOT}/bin:${PATH}"
-
 while IFS= read -r line || [ -n "$line" ]; do
-  PATH="${PYENV_ROOT}/versions/${line}/bin:${PATH}"
+  PYENV_PATH="${PYENV_PATH:+${PYENV_PATH}:}${PYENV_ROOT}/versions/${line}/bin"
 done <"${PYENV_ROOT}/version"
+
+# Reverse the path so that the first version in the file is first in $PATH
+PATH="${PYENV_PATH:+${PYENV_PATH}:}${PYENV_ROOT}/bin:${PATH}"
+unset PYENV_PATH
