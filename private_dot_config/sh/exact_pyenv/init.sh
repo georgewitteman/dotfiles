62a7b52091e68ac86d35b236998029c55cb1edc2
George Witteman <george@witteman.me>
2021-01-20 15:26:46 -0800

    Make a bunch of stuff posix compliant

--- private_dot_config/sh/exact_pyenv/init.sh
+++ private_dot_config/sh/exact_pyenv/init.sh
@@ -20,5 +20,5 @@ pyenv() {
 
   # Reverse the path so that the first version in the file is first in $PATH
   PATH="${PYENV_PATH:+${PYENV_PATH}:}${PYENV_ROOT}/bin:${PATH}"
-  unset PYENV_PATH NEW_PATH
+  unset PYENV_PATH
 }
--- private_dot_config/sh/exact_pyenv/profile.sh
+++ private_dot_config/sh/exact_pyenv/profile.sh
@@ -2,14 +2,12 @@
 
 export PYENV_ROOT="${HOME}/.local/share/pyenv"
 
-if [ ! -d "$PYENV_ROOT" ]; then
-  return
+if [ -d "$PYENV_ROOT" ]; then
+  while IFS= read -r line || [ -n "$line" ]; do
+    PYENV_PATH="${PYENV_PATH:+${PYENV_PATH}:}${PYENV_ROOT}/versions/${line}/bin"
+  done <"${PYENV_ROOT}/version"
+
+  # Reverse the path so that the first version in the file is first in $PATH
+  PATH="${PYENV_PATH:+${PYENV_PATH}:}${PYENV_ROOT}/bin:${PATH}"
+  unset PYENV_PATH
 fi
-
-while IFS= read -r line || [ -n "$line" ]; do
-  PYENV_PATH="${PYENV_PATH:+${PYENV_PATH}:}${PYENV_ROOT}/versions/${line}/bin"
-done <"${PYENV_ROOT}/version"
-
-# Reverse the path so that the first version in the file is first in $PATH
-PATH="${PYENV_PATH:+${PYENV_PATH}:}${PYENV_ROOT}/bin:${PATH}"
-unset PYENV_PATH
