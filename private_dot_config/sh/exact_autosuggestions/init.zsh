5973bc8c5adb2d291a46a8652a0e3b3211f346e3
George Witteman <george@witteman.me>
2021-03-13 10:04:01 -0800

    Install neovim

--- private_dot_config/sh/exact_autosuggestions/init.zsh
+++
@@ -1,7 +0,0 @@
-if [ ! -d "${XDG_CACHE_DIR:-${HOME}/.cache}/zsh-autosuggestions" ]; then
-  echo_run git clone git@github.com:zsh-users/zsh-autosuggestions.git \
-    "${XDG_CACHE_DIR:-${HOME}/.cache}/zsh-autosuggestions"
-fi
-
-ZSH_AUTOSUGGEST_USE_ASYNC=true
-source "${XDG_CACHE_DIR:-${HOME}/.cache}/zsh-autosuggestions/zsh-autosuggestions.zsh"
