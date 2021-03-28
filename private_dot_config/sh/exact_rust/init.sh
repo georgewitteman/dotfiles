451e16fb124e9598c94273c609d3d0028724f075
George Witteman <george@witteman.me>
2021-03-28 10:57:08 -0700

    Don't show rustup path

--- private_dot_config/sh/exact_rust/init.sh
+++ private_dot_config/sh/exact_rust/init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-if ! command -v rustup; then
+if ! command -v rustup >/dev/null 2>&1; then
   echo_eval 'curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path'
 fi
