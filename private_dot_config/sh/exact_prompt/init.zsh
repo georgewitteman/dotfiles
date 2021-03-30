d6c01ae47d7e07892ef338619e9a0600c3f91591
George Witteman <george@witteman.me>
2021-03-30 14:51:28 -0700

    Better handling of venvs with prompt

--- private_dot_config/sh/exact_prompt/init.zsh
+++ private_dot_config/sh/exact_prompt/init.zsh
@@ -1,3 +1,11 @@
+if [ -n "$VIRTUAL_ENV" ]; then
+  __zsh_prompt_venv="$VIRTUAL_ENV"
+  deactivate
+fi
 if command -v starship >/dev/null 2>/dev/null; then
   eval "$(starship init zsh)"
+fi
+if [ -n "$__zsh_prompt_venv" ]; then
+  source "${__zsh_prompt_venv}/bin/activate"
+  unset __zsh_prompt_venv
 fi
