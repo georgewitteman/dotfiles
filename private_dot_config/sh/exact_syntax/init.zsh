4a1072209d0c0c25b10b8ba0801ad269f2eee322
George Witteman <george@witteman.me>
2021-04-25 22:51:34 -0700

    Add fast syntax highlighting

--- private_dot_config/sh/exact_syntax/init.zsh
+++ private_dot_config/sh/exact_syntax/init.zsh
@@ -1,6 +1,6 @@
-if [ ! -d "${XDG_CACHE_DIR:-${HOME}/.cache}/zsh-syntax-highlighting" ]; then
-  echo_run git clone git@github.com:zsh-users/zsh-syntax-highlighting.git \
-    "${XDG_CACHE_DIR:-${HOME}/.cache}/zsh-syntax-highlighting"
+if [ ! -d "${XDG_CACHE_DIR:-${HOME}/.cache}/fast-syntax-highlighting" ]; then
+  echo_run git clone git@github.com:zdharma/fast-syntax-highlighting.git \
+    "${XDG_CACHE_DIR:-${HOME}/.cache}/fast-syntax-highlighting"
 fi
 
-source "${XDG_CACHE_DIR:-${HOME}/.cache}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+source "${XDG_CACHE_DIR:-${HOME}/.cache}/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
