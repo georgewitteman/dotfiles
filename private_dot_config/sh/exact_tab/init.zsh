8067b8bf5f945718af584a3f102f54e0fac069fc
George Witteman <george@witteman.me>
2021-05-19 15:56:27 -0700

    Comment out some tab-rs stuff

--- private_dot_config/sh/exact_tab/init.zsh
+++ private_dot_config/sh/exact_tab/init.zsh
@@ -1,3 +1,3 @@
-if [ -f "${HOME}/Library/Application Support/tab/completion/zsh-history.zsh" ]; then
-  source "${HOME}/Library/Application Support/tab/completion/zsh-history.zsh"
-fi
+# if [ -f "${HOME}/Library/Application Support/tab/completion/zsh-history.zsh" ]; then
+#   source "${HOME}/Library/Application Support/tab/completion/zsh-history.zsh"
+# fi
