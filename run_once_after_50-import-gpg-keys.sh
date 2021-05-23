abc77aed4744783ff02e74f0fbc38a3458b61a5d
George Witteman <george@witteman.me>
2021-05-23 16:06:48 -0700

    Move some bootstrap scripts to chezmoi scripts

--- run_once_after_50-import-gpg-keys.sh
+++ run_once_after_50-import-gpg-keys.sh
@@ -2,5 +2,7 @@
 
 set -e
 
+echo-info "Importing personal GPG keys"
+
 echo-eval 'op get document "Personal GPG Key - George Witteman (AE0381FB) – Public.asc" | gpg --batch --import'
 echo-eval 'op get document "Personal GPG Key - George Witteman (AE0381FB) – Secret.asc" | gpg --batch --import'
