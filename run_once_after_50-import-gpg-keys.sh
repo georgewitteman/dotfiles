a3ded0aa7293d21b70b0c69ca91a0fe9a5aa8355
George Witteman <george@witteman.me>
2021-05-23 17:29:33 -0700

    Improve initial install for GPG and 1password cli

--- run_once_after_50-import-gpg-keys.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-echo-info "Importing personal GPG keys"
-
-echo-eval 'op get document "Personal GPG Key - George Witteman (AE0381FB) – Public.asc" | gpg --batch --import'
-echo-eval 'op get document "Personal GPG Key - George Witteman (AE0381FB) – Secret.asc" | gpg --batch --import'
---
+++ run_once_before_20-install-1password-cli.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo-run brew install --cask 1password-cli
