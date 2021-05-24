aec0412a56136afa273b80729ff3d0c16477e299
George Witteman <george@witteman.me>
2021-05-23 20:25:39 -0700

    Setup SSH key for GitHub

--- private_dot_config/sh/exact_autocomplete/init.zsh
+++
@@ -1,6 +0,0 @@
-# if [ ! -d "${XDG_CACHE_DIR:-${HOME}/.cache}/zsh-autocomplete" ]; then
-#   echo_run git clone git@github.com:marlonrichert/zsh-autocomplete.git \
-#     "${XDG_CACHE_DIR:-${HOME}/.cache}/zsh-autocomplete"
-# fi
-
-# source "${XDG_CACHE_DIR:-${HOME}/.cache}/zsh-autocomplete/zsh-autocomplete.plugin.zsh"
