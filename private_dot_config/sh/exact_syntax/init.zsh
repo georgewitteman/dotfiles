9c539c0edcdad9e8524ce0aa404d7549766d9495
George Witteman <george@witteman.me>
2021-05-23 21:26:36 -0700

    Fix zsh init for build

--- private_dot_config/sh/exact_syntax/init.zsh
+++ private_dot_config/sh/exact_syntax/init.zsh
@@ -1,6 +1,12 @@
+if [ -n "$CI" ]; then
+  set -x
+fi
+
 if [ ! -d "${XDG_CACHE_DIR:-${HOME}/.cache}/fast-syntax-highlighting" ]; then
   echo_run git clone git@github.com:zdharma/fast-syntax-highlighting.git \
     "${XDG_CACHE_DIR:-${HOME}/.cache}/fast-syntax-highlighting"
 fi
 
-source "${XDG_CACHE_DIR:-${HOME}/.cache}/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
+if [ -t 0 ] && [ -t 1 ]; then
+  source "${XDG_CACHE_DIR:-${HOME}/.cache}/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
+fi
