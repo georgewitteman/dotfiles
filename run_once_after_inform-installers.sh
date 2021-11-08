36584bc43c2698976f70dee577a1b00f67f5e4ec
George Witteman <george@witteman.me>
2021-11-07 20:12:29 -0800

    Use ssh repo

--- run_once_after_inform-installers.sh
+++ run_once_after_inform-installers.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 echo "Installed base files!"
+echo "Restart your terminal 'exec ${SHELL} -li'"
 echo "You can now run specific installers for any programs you need:"
 ls ~/bin/install* | xargs -n 1 basename | xargs -n 1 echo " "
