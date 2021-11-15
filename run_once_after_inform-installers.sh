f9b5cfb05478e24edf6191997b59e2f51517068a
George Witteman <george@witteman.me>
2021-11-14 22:15:28 -0800

    Update run_once_after_inform-installers.sh

--- run_once_after_inform-installers.sh
+++ run_once_after_inform-installers.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-echo "Installed base files!"
-echo
 echo "Use ssh repo URL for dotfiles repo:"
 echo '  $ (cd "$(chezmoi source-path)" && git-https-to-ssh)'
 echo
 echo "You can now run specific installers for any programs you need:"
-ls ~/bin/install* | xargs -n 1 basename | xargs -n 1 echo " "
+ls ~/bin/install* | xargs -n 1 basename | xargs -n 1 printf "  $ %s\n"
 echo
-echo "Restart your terminal 'exec ${SHELL:-/bin/bash} -li'"
+echo "Restart your terminal:"
+echo "  $ exec ${SHELL:-/bin/bash} -li"
