f91687bd953b6eea4c34cb9637031825a4402ec9
George Witteman <george@witteman.me>
2021-11-20 23:58:24 -0800

    Use externals and templates for more stuff

--- private_dot_config/exact_sh/exact_syntax/init.zsh
+++ private_dot_config/exact_sh/exact_syntax/init.zsh
@@ -1,3 +1,3 @@
-if [ -t 0 ] && [ -t 1 ] && [ -d "${XDG_CACHE_DIR:-${HOME}/.cache}/fast-syntax-highlighting" ]; then
-  source "${XDG_CACHE_DIR:-${HOME}/.cache}/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
+if [ -t 0 ] && [ -t 1 ]; then
+  source "${HOME}/.cache/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
 fi
