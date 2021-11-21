0aa205d7e69b9892b2b5d072da0d8f121f493854
George Witteman <george@witteman.me>
2021-11-21 06:01:41 +0000

    Move rustup installer to script

--- private_dot_config/exact_sh/exact_rust/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if ! command -v rustup >/dev/null 2>&1; then
-  rustup() {
-    safe-eval 'curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path'
-  }
-fi
