c0dd46f5f900fd70535422f4a5c5a54563863f87
George Witteman <george@witteman.me>
2021-12-08 13:55:15 -0800

    Remove syntax highlighting plugin

--- private_dot_config/exact_sh/exact_syntax/init.zsh
+++ private_dot_config/exact_sh/exact_syntax/init.zsh
@@ -1,3 +1,3 @@
-if [ -t 0 ] && [ -t 1 ]; then
+if [ -t 0 ] && [ -t 1 ] && [ -f "${HOME}/.cache/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh" ]; then
   source "${HOME}/.cache/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
 fi
