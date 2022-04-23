f8e2f6122b14252c3f0af50b9132bd7ea8a09119
George Witteman <george@witteman.me>
2022-04-22 23:01:14 -0700

    Remove accidental duplicate install-dependencies.sh script

--- run_once_before_install-dependencies.sh
+++ run_once_before_install-dependencies.sh
@@ -17,7 +17,7 @@ if command -v brew >/dev/null 2>&1; then
 elif command -v apt-get >/dev/null 2>&1; then
   sudo apt-get install git
 elif command -v apk >/dev/null 2>&1; then
-  sudo apk install git
+  sudo apk add git
 else
   echo "Don't know how to install git."
   exit 1
