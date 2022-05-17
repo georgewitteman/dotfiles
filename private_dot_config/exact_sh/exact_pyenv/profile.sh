2880480a4be331f2508ca698c98cff2ed1b4b259
George Witteman <george@witteman.me>
2022-05-17 00:58:30 +0000

    Use standard pyenv install

--- private_dot_config/exact_sh/exact_pyenv/profile.sh
+++ private_dot_config/exact_sh/exact_pyenv/profile.sh
@@ -2,23 +2,9 @@
 
 export PYENV_ROOT="${PYENV_ROOT:-"${HOME}/.pyenv"}"
 
-if [ -n "$PYENV_VERSION" ] && [ -d "${PYENV_ROOT}/versions/${PYENV_VERSION}" ]; then
-  PYENV_PATH="${PYENV_PATH:+${PYENV_PATH}:}${PYENV_ROOT}/versions/${PYENV_VERSION}/bin"
-elif [ -f "${PWD}/.python-version" ]; then
-  while IFS= read -r line || [ -n "$line" ]; do
-    PYENV_PATH="${PYENV_PATH:+${PYENV_PATH}:}${PYENV_ROOT}/versions/${line}/bin"
-  done <"${PWD}/.python-version"
-elif [ -f "${PYENV_ROOT}/version" ]; then
-  while IFS= read -r line || [ -n "$line" ]; do
-    PYENV_PATH="${PYENV_PATH:+${PYENV_PATH}:}${PYENV_ROOT}/versions/${line}/bin"
-  done <"${PYENV_ROOT}/version"
+if ! command -v pyenv >/dev/null 2>&1; then
+  PATH="${PYENV_ROOT}/bin:${PATH}"
 fi
 
-# Reverse the path so that the first version in the file is first in $PATH
-PATH="${PYENV_PATH:+${PYENV_PATH}:}${PYENV_ROOT}/bin:${PATH}"
-unset PYENV_PATH
 
-if [ -n "$VIRTUAL_ENV" ]; then
-  # shellcheck source=/dev/null
-  . "${VIRTUAL_ENV}/bin/activate"
-fi
+eval "$(pyenv init - --path --no-rehash)"
