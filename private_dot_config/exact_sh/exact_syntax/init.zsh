751cfe4b1aefc8ffdf0e8f553f68324071421528
George Witteman <george@witteman.me>
2022-05-17 04:00:07 +0000

    Delete syntax shell plugin

--- private_dot_config/exact_sh/exact_syntax/init.zsh
+++
@@ -1,3 +0,0 @@
-if [ -t 0 ] && [ -t 1 ] && [ -f "${HOME}/.cache/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh" ]; then
-  source "${HOME}/.cache/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
-fi
