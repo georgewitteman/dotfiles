aa0d263cd2d1f96313c1bdf04b7d06bb5d873eef
George Witteman <george@witteman.me>
2022-05-17 03:59:48 +0000

    Remove nvm_upgrade function

--- private_dot_config/exact_sh/exact_nvm/init.sh
+++ private_dot_config/exact_sh/exact_nvm/init.sh
@@ -1,23 +1,9 @@
 #!/bin/sh
 
+# Lazy loads nvm
 nvm() {
-  if [ ! -d "$NVM_DIR" ]; then
-    nvm_upgrade
-  fi
   unset -f nvm
   # shellcheck source=/dev/null disable=SC2240
   . "$NVM_DIR/nvm.sh" --no-use
   nvm "$@"
-}
-
-nvm_upgrade() {
-  if [ ! -d "$NVM_DIR" ]; then
-    echo "nvm is not installed. Do you want to install it?"
-    safe-run git clone "https://github.com/nvm-sh/nvm.git" "$NVM_DIR"
-  else
-    echo_run git -C "$NVM_DIR" fetch
-  fi
-  echo_run git -C "$NVM_DIR" checkout \
-    "$(git -C "$NVM_DIR" describe --abbrev=0 --tags --match "v[0-9]*" \
-      "$(git -C "$NVM_DIR" rev-list --tags --max-count=1)")"
 }
