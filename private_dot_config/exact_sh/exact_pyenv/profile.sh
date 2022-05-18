d456fdc223a1faa719708ab7f0cbc0fbe0e6ec6f
George Witteman <george@witteman.me>
2022-05-18 05:19:54 +0000

    Revert "Use standard pyenv install"
    This reverts commit 2880480a4be331f2508ca698c98cff2ed1b4b259.

--- private_dot_config/exact_sh/exact_pyenv/profile.sh
+++ private_dot_config/exact_sh/exact_pyenv/profile.sh
@@ -2,8 +2,23 @@
 
 export PYENV_ROOT="${PYENV_ROOT:-"${HOME}/.pyenv"}"
 
-if ! command -v pyenv >/dev/null 2>&1; then
-  PATH="${PYENV_ROOT}/bin:${PATH}"
+if [ -n "$PYENV_VERSION" ] && [ -d "${PYENV_ROOT}/versions/${PYENV_VERSION}" ]; then
+  PYENV_PATH="${PYENV_PATH:+${PYENV_PATH}:}${PYENV_ROOT}/versions/${PYENV_VERSION}/bin"
+elif [ -f "${PWD}/.python-version" ]; then
+  while IFS= read -r line || [ -n "$line" ]; do
+    PYENV_PATH="${PYENV_PATH:+${PYENV_PATH}:}${PYENV_ROOT}/versions/${line}/bin"
+  done <"${PWD}/.python-version"
+elif [ -f "${PYENV_ROOT}/version" ]; then
+  while IFS= read -r line || [ -n "$line" ]; do
+    PYENV_PATH="${PYENV_PATH:+${PYENV_PATH}:}${PYENV_ROOT}/versions/${line}/bin"
+  done <"${PYENV_ROOT}/version"
 fi
 
-eval "$(pyenv init - --path --no-rehash)"
+# Reverse the path so that the first version in the file is first in $PATH
+PATH="${PYENV_PATH:+${PYENV_PATH}:}${PYENV_ROOT}/bin:${PATH}"
+unset PYENV_PATH
+
+if [ -n "$VIRTUAL_ENV" ]; then
+  # shellcheck source=/dev/null
+  . "${VIRTUAL_ENV}/bin/activate"
+fi
