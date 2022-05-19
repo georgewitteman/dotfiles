c2f1380d3a7a3088ea089c3f294078f747d56591
George Witteman <george@witteman.me>
2022-05-19 15:15:28 -0700

    Fix nvm initialization

--- .chezmoitemplates/nvm/profile.sh
+++ .chezmoitemplates/nvm/profile.sh
@@ -4,7 +4,7 @@ export NVM_DIR="${NVM_DIR:-"${HOME}/.nvm"}"
 if [ -f "${NVM_DIR}/alias/default" ]; then
   read -r line <"${NVM_DIR}/alias/default"
 
-  for node_bin in "${NVM_DIR}/versions/node/v${line}"*"/bin"; do
+  for node_bin in "${NVM_DIR}/versions/node/v${line#v}"*"/bin"; do
     [ -d "$node_bin" ] || continue
     PATH="${node_bin}:${PATH}"
     break
