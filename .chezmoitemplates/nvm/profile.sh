716e6faafc7e0f536eb165a7993fa929346e22e0
George Witteman <george@witteman.me>
2022-08-17 11:08:50 -0700

    Support .node-version file when loading nvm

--- .chezmoitemplates/nvm/profile.sh
+++ .chezmoitemplates/nvm/profile.sh
@@ -1,8 +1,7 @@
 export NVM_DIR="${NVM_DIR:-"${HOME}/.nvm"}"
 
-# Load default node version. It's done like this to stay performant.
-if [ -f "${NVM_DIR}/alias/default" ]; then
-  read -r line <"${NVM_DIR}/alias/default"
+load_file() {
+  read -r line <"$1"
 
   for node_bin in "${NVM_DIR}/versions/node/v${line#v}"*"/bin"; do
     [ -d "$node_bin" ] || continue
@@ -12,4 +11,13 @@ if [ -f "${NVM_DIR}/alias/default" ]; then
 
   unset line
   unset node_bin
+}
+
+# Load default node version. It's done like this to stay performant.
+if [ -f "${PWD}/.node-version" ]; then
+  load_file "${PWD}/.node-version"
+elif [ -f "${NVM_DIR}/alias/default" ]; then
+  load_file "${NVM_DIR}/alias/default"
 fi
+
+unset -f load_file
