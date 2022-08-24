642164e223aaa16b41ad0e1809a11d474b4360a5
George Witteman <george@witteman.me>
2022-08-24 01:47:04 +0000

    Add dev container config

---
+++ run_devcontainers.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -o errexit
+
+if [ "$REMOTE_CONTAINERS" != "true" ]; then
+  echo "skipping dev container setup (not in dev container)" >&2
+  exit
+fi
+
+if ! sudo --validate --non-interactive >/dev/null 2>&1; then
+  echo "skipping dev container setup that requires sudo" >&2
+  exit
+fi
+
+if command -v curl >/dev/null 2>&1; then
+  if ! command -v starship >/dev/null 2>&1; then
+    curl -sS https://starship.rs/install.sh | sh -s -- --yes --verbose
+  fi
+else
+  echo "skipping starship install (no curl)" >&2
+fi
+
+if command -v apt-get >/dev/null 2>&1; then
+  if ! command -v fzf >/dev/null 2>&1; then
+    sudo apt-get update
+    sudo apt-get install fzf
+  fi
+else
+  echo "skipping fzf install (no apt-get)" >&2
+fi
