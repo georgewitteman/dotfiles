eeb93f48b7196272c991639381a3e3d0ad186067
George Witteman <george@witteman.me>
2022-09-26 16:24:36 -0700

    Automatically install terraform completions

---
+++ .chezmoitemplates/terraform-completion.bash
@@ -0,0 +1,5 @@
+# https://github.com/posener/complete/blob/v1/cmd/install/bash.go
+# https://github.com/posener/complete/blob/9a4745ac49b29530e07dc2581745a218b646b7a3/cmd/install/install.go#L86-L89
+{{- if lookPath "terraform" }}
+complete -C {{ lookPath "terraform" }} terraform
+{{ end -}}
