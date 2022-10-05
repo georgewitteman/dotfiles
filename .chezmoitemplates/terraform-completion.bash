8e8f6dc91676773013e1122d4937aaae0b2be3b9
George Witteman <george@witteman.me>
2022-10-04 20:52:32 -0700

    Fix lint

--- .chezmoitemplates/terraform-completion.bash
+++ .chezmoitemplates/terraform-completion.bash
@@ -2,4 +2,4 @@
 # https://github.com/posener/complete/blob/9a4745ac49b29530e07dc2581745a218b646b7a3/cmd/install/install.go#L86-L89
 {{- if lookPath "terraform" }}
 complete -C {{ lookPath "terraform" }} terraform
-{{ end -}}
+{{- end -}}
