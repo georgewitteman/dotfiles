6906b30b591802fa950542f0d266ceacdc614666
George Witteman <george@witteman.me>
2022-11-01 16:31:34 -0700

    Clean up

--- home/.chezmoitemplates/nvm/init.bash
+++
@@ -1,4 +0,0 @@
-if [[ -s "${NVM_DIR}/bash_completion" ]]; then
-  # shellcheck source=/dev/null
-  \. "${NVM_DIR}/bash_completion"
-fi
--- home/.chezmoitemplates/nvm/init.sh
+++
@@ -1,7 +0,0 @@
-# Lazy load nvm.
-nvm() {
-  unset -f nvm
-  # shellcheck source=/dev/null disable=SC2240
-  . "$NVM_DIR/nvm.sh" --no-use
-  nvm "$@"
-}
