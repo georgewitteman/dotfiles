2cfa49507af778eb411d36297421dc9bcddb3dc6
George Witteman <george@witteman.me>
2022-11-01 16:53:51 -0700

    More cleanup

--- home/.chezmoitemplates/prompt/init.bash
+++
@@ -1,6 +0,0 @@
-if command -v starship >/dev/null 2>/dev/null; then
-  # shellcheck disable=SC2312
-  eval "$(starship init bash)"
-else
-  PS1='\[\033[01;34m\]\w\[\033[00m\] \$ '
-fi
--- home/.chezmoitemplates/prompt/init.sh
+++
@@ -1,7 +0,0 @@
-if [ "$(id -u || true)" -eq 0 ]; then
-  PS1='$PWD $'
-else
-  PS1='$PWD $ '
-fi
-PS2='> '
-PS4='+ '
